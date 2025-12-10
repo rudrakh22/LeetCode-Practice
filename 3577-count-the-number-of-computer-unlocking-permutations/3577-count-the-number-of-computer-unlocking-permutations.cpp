8468347997f523632cbad527693f36d3a1022397
Rudraksh Gupta <rudrakshg1914@gmail.com>
2025-12-10 15:03:34 +0530

    Time: 3 ms (43.5%), Space: 92.9 MB (23.5%) - LeetHub

---
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countPermutations(vector<int>& complexity) {
+        int MOD=1e9+7;
+        long long ans=1;
+        for(int i=1;i<complexity.size();i++){
+            if(complexity[i]<=complexity[0]) return 0;
+            ans=(ans*i)%MOD;
+        }
+        return (int)ans;
+    }
+};
