e79f664d4c92b2d05698fd78e346cf8f977f1ae5
Rudraksh Gupta <rudrakshg1914@gmail.com>
2026-01-07 16:13:00 +0530

    Time: 5 ms (43.43%), Space: 79 MB (25.12%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -1,34 +1,28 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// Variant with an extra compute_ans=0 in dfs
 class Solution {
 public:
-    long long totalProduct=0;
-    const int MOD=1e9+7;
-    long long dfs(TreeNode* root){
-        if(!root) return 0;
-        return root->val+dfs(root->left)+dfs(root->right);
+    const int mod=1e9+7;
+    long long ans=INT_MIN, total=0;
+    int dfs(TreeNode* root, bool compute_ans=0){
+        if (!root) return 0;
+        int sum=root->val
+            +dfs(root->left, compute_ans)
+            +dfs(root->right, compute_ans);
+        if (compute_ans) ans=max(ans, (total-sum)*sum);
+        return sum;
     }
-    long long dfsSub(TreeNode * root,long long total){
-        if(!root) return 0;
-        long long leftSum=dfsSub(root->left,total);
-        long long rightSum=dfsSub(root->right,total);
-        long long subSum=leftSum+rightSum+root->val;
-        totalProduct=max(totalProduct,subSum*(total-subSum));
-        return subSum;
-    }
-
     int maxProduct(TreeNode* root) {
-        long long total=dfs(root);
-        dfsSub(root,total);
-        return totalProduct%MOD;
+        total=dfs(root);
+        dfs(root, 1);
+        return ans%mod;
     }
 };
+
+
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
