07fe9461e92f54f579022c39ad6375d6aecfccf1
Rudraksh Gupta <rudrakshg1914@gmail.com>
2026-03-03 16:51:08 +0530

    Time: 0 ms (100%), Space: 7.8 MB (63.07%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        int invertCount = 0;
-        int len = (1 << n) - 1;  // Length of Sn is 2^n - 1
-
-        while (k > 1) {
-            // If k is in the middle, return based on inversion count
-            if (k == len / 2 + 1) {
-                return invertCount % 2 == 0 ? '1' : '0';
+        int invertCount=0;
+        int len=(1<<n)-1;
+        while(k>1){
+            if(len/2+1==k){
+                return invertCount%2==0 ? '1':'0'; 
             }
-
-            // If k is in the second half, invert and mirror
-            if (k > len / 2) {
-                k = len + 1 - k;  // Mirror position
-                invertCount++;    // Increment inversion count
+            if(k>len/2){
+                k=len+1-k;
+                invertCount++;
             }
-
-            len /= 2;  // Reduce length for next iteration
+            len/=2;
         }
-
-        // For the first position, return based on inversion count
-        return invertCount % 2 == 0 ? '0' : '1';
+        return invertCount%2==0 ? '0': '1';
     }
 };
