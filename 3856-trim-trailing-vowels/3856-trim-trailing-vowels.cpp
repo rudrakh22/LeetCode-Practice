caa5407fb09e024dc058ea26ea03edffe3274d12
Rudraksh Gupta <rudrakshg1914@gmail.com>
2026-03-03 17:07:02 +0530

    Time: 0 ms (100%), Space: 8.9 MB (17.53%) - LeetHub

---
+++ 3856-trim-trailing-vowels/3856-trim-trailing-vowels.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        int n=s.size();
+        for(int i=n-1;i>=0;i--){
+            if(s[i]!='a' && s[i]!='e' && s[i]!='i' && s[i]!='o' && s[i]!='u' ) {
+                return s.substr(0,i+1);
+            }
+        }
+        return "";
+    }
+};
