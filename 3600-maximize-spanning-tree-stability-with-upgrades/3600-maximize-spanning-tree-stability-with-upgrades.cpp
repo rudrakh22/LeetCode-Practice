353b0286331e2a3780d734a1542b9272efb7ab12
Rudraksh Gupta <rudrakshg1914@gmail.com>
2026-03-12 16:14:42 +0530

    Time: 771 ms (8.63%), Space: 404.5 MB (8.63%) - LeetHub

--- 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3600-maximize-spanning-tree-stability-with-upgrades/3600-maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,41 +1,40 @@
-class DSU {
-public:
-    vector<int> parent, rankv;
-
-    DSU(int n) {
+class DSU{
+    
+    vector<int>parent;
+    vector<int>rank;
+    public:
+    DSU(int n){
         parent.resize(n);
-        rankv.resize(n,0);
+        rank.resize(n,0);
         for(int i=0;i<n;i++) parent[i]=i;
     }
-
     int find(int x){
-        if(parent[x]!=x)
+        if(parent[x] !=x){
             parent[x]=find(parent[x]);
+        }
         return parent[x];
     }
-
     bool unite(int a,int b){
-        int pa=find(a), pb=find(b);
+        int pa=find(a),pb=find(b);
         if(pa==pb) return false;
-
-        if(rankv[pa]<rankv[pb]) parent[pa]=pb;
-        else if(rankv[pb]<rankv[pa]) parent[pb]=pa;
+        if(rank[pa]<rank[pb]) parent[pa]=pb;
+        else if(rank[pa] > rank[pb]) parent[pb]=pa;
         else{
             parent[pb]=pa;
-            rankv[pa]++;
+            rank[pa]++;
         }
         return true;
     }
 };
+
 class Solution {
 public:
     bool isPossible(int n,vector<vector<int>>& edges,int k,int val){
         DSU dsu(n);
         int used=0;
-        vector<vector<int>> good;
-        vector<vector<int>> upgrade;
-
-        for(auto & e:edges){
+        vector<vector<int>>good;
+        vector<vector<int>>upgrade;
+        for(auto &e:edges){
             int u=e[0],v=e[1],s=e[2],must=e[3];
             if(must==1){
                 if(s<val) return false;
@@ -43,41 +42,37 @@ public:
                 used++;
             }else{
                 if(s>=val) good.push_back(e);
-                else if(2*s>=val) upgrade.push_back(e);
+                else if (2*s >=val) upgrade.push_back(e);
             }
         }
-        for(auto &e:good){
-            if(dsu.unite(e[0],e[1])){
+        for(auto &edge:good){
+            if(dsu.unite(edge[0],edge[1])) {
                 used++;
                 if(used==n-1) return true;
             }
         }
-
         int upgrades=0;
-        for(auto &e:upgrade){
+        for(auto &edge:upgrade){
             if(upgrades==k) break;
-
-            if(dsu.unite(e[0],e[1])){
-                upgrades++;
+            if(dsu.unite(edge[0],edge[1])) {
                 used++;
+                upgrades++;
                 if(used==n-1) return true;
             }
         }
 
-        return used==n-1;
-
+        return used==n-1; 
     }
     int maxStability(int n, vector<vector<int>>& edges, int k) {
         int maxi=0;
-        for(auto &edge : edges) maxi=max(maxi,edge[2]);
+        for(auto &edge:edges) maxi=max(maxi,edge[2]);
         int left=0,right=2*maxi,ans=-1;
         while(left<=right){
             int mid=(left+right)/2;
             if(isPossible(n,edges,k,mid)){
                 ans=mid;
                 left=mid+1;
-            }
-            else right=mid-1;
+            }else right=mid-1;
         }
         return ans;
     }
